3ebe5291f378175ce3bff9f288fa252413156ff1
GuodaBal <az.guodabaltutyte@gmail.com>
2024-08-26 13:50:21 +0300

    login

--- LunchTests/tests/test1.spec.js
+++ LunchTests/tests/test1.spec.js
@@ -1,11 +1,13 @@
 import { test, expect } from "@playwright/test";
 
+var email = "guoda.baltutyte@sft.com";
+var password = "student330";
 // Task - 1: Login as user
 
 test("Create new computer", async ({ page }) => {
   await page.goto("https://lunch.devbstaging.com/login-password");
 
-  await page.getByLabel("email").fill("ABC");
-  
+  await page.getByLabel("email").fill(email);
+  await page.getByLabel("password").fill(password);
   expect(1).toBe(1);
 });
